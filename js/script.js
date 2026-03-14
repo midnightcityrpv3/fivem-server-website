ef08efd648d5ee3404b447c5c0c5325f5a432eb8
midnightcityrpv3 <j.m.timermanis18@gmail.com>
2026-03-14 15:51:58 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+function showForm(department){
 
+document.querySelectorAll(".department").forEach(form=>{
+form.classList.remove("active")
+})
+
+document.getElementById(department).classList.add("active")
+
+document.querySelectorAll(".tab").forEach(tab=>{
+tab.classList.remove("active")
+})
+
+event.target.classList.add("active")
+
+}
